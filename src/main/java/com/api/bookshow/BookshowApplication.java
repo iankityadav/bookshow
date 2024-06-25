02d0c60dbb5c7346e00d0d03368a11f2e6a38527
iankityadav <1002ankit@gmail.com>
2024-06-25 20:34:31 +0530

    first commit

---
+++ src/main/java/com/api/bookshow/BookshowApplication.java
@@ -0,0 +1,13 @@
+package com.api.bookshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/bookshow/BookshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bookshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
