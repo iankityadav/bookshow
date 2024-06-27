f680458c764d5602bcee73ae4c558af08bf8288a
iankityadav <1002ankit@gmail.com>
2024-06-27 11:10:37 +0530

    coupon creation service fix

--- src/main/java/com/api/bookshow/controller/CouponController.java
+++ src/main/java/com/api/bookshow/controller/CouponController.java
@@ -3,6 +3,7 @@ package com.api.bookshow.controller;
 import com.api.bookshow.model.Coupon;
 import com.api.bookshow.service.CouponService;
 import jakarta.annotation.security.RolesAllowed;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CouponController {
 
+    @Autowired
     private CouponService couponService;
 
     @RolesAllowed("ADMIN")
